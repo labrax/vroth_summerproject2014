1854f3933ca684a8a16d9f45d70b6de688b939da
labrax <labrax@gmail.com>
2014-06-26 18:11:50 +0100

    moved large itemsets class

---
+++ apriori/src/large.cpp
@@ -0,0 +1,51 @@
+/* author: Victor Roth Cardoso - vroth
+ * student sponsored by CNPq
+ * 
+ * in summer project at Aberystwyth University
+ * 
+ * 
+ * supervisors: Robert Hoehndorf and Georgios Gkoutos
+ */
+ 
+#include "large.hpp"
+
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+LargeItemSet::LargeItemSet(unsigned int iteration) {
+	this->k = iteration;
+}
+
+LargeItemSet::~LargeItemSet() {
+	for(auto &i : itemset) {
+		delete(i);
+	}
+}
+
+void LargeItemSet::insertSet(ItemSet * set) {
+	itemset.insert(itemset.end(), set);
+}
+
+vector<ItemSet *> & LargeItemSet::getItemSets() {
+	return itemset;
+}
+
+unsigned int LargeItemSet::getInteration() {
+	return k;
+}
+
+void LargeItemSet::print() {
+	for(auto &i : itemset) {
+		cout << "With support " << i->getSupportCount() << ": ";
+		for(auto &j: i->getItemSet()) {
+			cout << j.first << " ";
+		}
+		cout << endl;
+	}
+}
+
+void LargeItemSet::printinfo() {
+	cout << "iteraction number: " << this->getInteration() << " has " << this->getItemSets().size() << " itemsets" << endl;
+}
---
+++ apriori/src/large.hpp
@@ -0,0 +1,35 @@
+/* author: Victor Roth Cardoso - vroth
+ * student sponsored by CNPq
+ * 
+ * in summer project at Aberystwyth University
+ * 
+ * 
+ * supervisors: Robert Hoehndorf and Georgios Gkoutos
+ */
+
+/** a general class for storing a set of k-itemsets
+ */
+
+#pragma once
+
+#include "itemset.hpp"
+
+#include <vector>
+
+using std::vector;
+
+class LargeItemSet {
+	private:
+		vector<ItemSet *> itemset;
+		
+		unsigned int k; //to identify the iteration, also the size
+	public:
+		LargeItemSet(unsigned int);
+		~LargeItemSet();
+		void insertSet(ItemSet * set);
+		
+		vector<ItemSet *> & getItemSets();
+		unsigned int getInteration();
+		void print();
+		void printinfo();
+};
