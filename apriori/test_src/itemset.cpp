3a854f3927cfc04d2ffa58b2efa90e8773cf7b13
labrax <labrax@gmail.com>
2014-06-26 20:08:43 +0100

    test files

---
+++ apriori/test_src/itemset.cpp
@@ -0,0 +1,38 @@
+
+/** testing code
+ * 
+ */
+
+#include "../src/itemset.hpp"
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main(void) {
+	ItemSet a;
+	a.insert("A");
+	a.insert("B");
+	a.insert("C");
+	
+	if(a.contains(string("B"))) {
+		cout << "contains B" << endl;
+	}
+	
+	if(a.contains(string("D"))) {
+		cout << "contains D" << endl;
+	}
+	
+	if(a.contains(string("A"))) {
+		cout << "contains A" << endl;
+	}
+	
+	for(auto & i: a.subItemSets()) {
+		cout << "-> ";
+		for(auto &j: i->getItemSet()) {
+			cout << j.first << " ";
+		}
+		cout << endl;
+		delete(i);
+	}
+}
