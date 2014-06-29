6481adea5fb62f87157558105d259e0a48cfce0e
labrax <labrax@gmail.com>
2014-06-29 16:29:33 +0100

    new thread function for CandidateItemSet

--- apriori/src/candidate/candidate.cpp
+++ apriori/src/candidate/candidate.cpp
@@ -91,6 +91,120 @@ LargeItemSet * CandidateItemSet::apriori_gen(LargeItemSet * a) {
 	return new_candidate;
 }
 
+void run_apriori_genThreaded(LargeItemSet * new_candidate, LargeItemSet * a, vector<ItemSet *> * last_large, unsigned int rangeLow, unsigned int rangeHigh) {	
+	for(unsigned int i=rangeLow; i<rangeHigh; i++) {
+		ItemSet * first = (*last_large)[i];
+		for(unsigned int j=i+1; j<last_large->size(); j++) {
+			ItemSet * second = (*last_large)[j];
+			
+			//!this will test if they are similar until the last element
+			bool nice = true;
+			
+			string last_first = first->getItemSet().rbegin()->first;
+			map<string, bool>::iterator it = first->getItemSet().begin();
+			map<string, bool>::iterator it2 = second->getItemSet().begin();
+			
+			while(it->first != last_first) { //while is not the last
+				if(it->first != it2->first) {//its differente, we are not cool
+					nice = false;
+					break;
+				}
+				it++; //increment both iterators
+				it2++;
+			}
+			//!this will test if they are similar until the last element
+			
+			if(nice == true) {
+				ItemSet * new_itemset = new ItemSet(first); //create new itemset
+				new_itemset->insert(it2->first); //with new element
+				
+				bool ok = true; //!this block will prune candidates
+				vector<ItemSet *> subsets = new_itemset->subItemSets(); //get the subsets (without empty and full)
+				
+				for(auto &s : subsets) { //verify if all the subsets, ItemSets exists on the large
+					if(s->getAmountElements() > 1) { //if has size 1 can be skipped
+						if(a->contains(s) == false) { //if doesnt contain, doesn't add
+							ok = false;
+							break;
+						}
+					}
+				}
+				for(auto &s: subsets)
+					delete(s); //after verifying the subsets delete it
+					
+				
+				//cout << "prune results were " << ok << endl;
+				
+				if(ok) 
+					new_candidate->insertSet(new_itemset); //!this block will prune candidates
+				else
+					delete(new_itemset); //if the candidate itemset is not used delete the memory allocated
+			}
+		}
+	}
+	//cout << "thread exited" << endl;
+}
+
+inline unsigned int amount_op_aux(unsigned int a, unsigned int b) {
+	return ((b*b*b-a*a*a) - (b-a))/3;
+}
+
+inline unsigned int amount_op(unsigned int amount_transactions, unsigned int block_init, unsigned int block_end) { //inside is a quadratic function
+	return amount_op_aux(block_init, amount_transactions) - amount_op_aux(block_end, amount_transactions);
+}
+
+LargeItemSet * CandidateItemSet::apriori_genThreaded(LargeItemSet * a) {
+	LargeItemSet * new_candidate = new LargeItemSet(a->getIteration()+1);
+	vector<ItemSet *> last_large = a->getItemSets();
+	
+	unsigned concurentThreadsSupported = thread::hardware_concurrency();
+	
+	vector <thread *> threads;
+	/*vector <LargeItemSet *> new_candidate_thread;
+	for(unsigned int i=0; i < concurentThreadsSupported; i++) {
+		new_candidate_thread.insert(new_candidate_thread.end(), new LargeItemSet(a->getIteration()+1));
+	}*/
+	
+	unsigned int NUM_EACH_THREAD = amount_op(a->getAmountTransactions(), 0, a->getAmountTransactions())/concurentThreadsSupported;
+	
+	//cout << "transactions: " << a->getAmountTransactions() << endl;
+	
+	unsigned int block_init = 0, block_end;
+	while(threads.size() < concurentThreadsSupported-1 && block_init != a->getAmountTransactions()) {
+		block_end = block_init;
+		
+		while(amount_op(a->getAmountTransactions(), block_init, block_end) < NUM_EACH_THREAD)
+			block_end++;
+
+		/*block_end--; //try to compensate
+		if(block_end == block_init)
+			block_end++;*/
+		
+		//cout << "thread: " << block_init << ", " << block_end << endl;
+		threads.insert(threads.end(), new thread(run_apriori_genThreaded, new_candidate/*_thread[threads.size()]*/, a, &last_large, block_init, block_end));
+		
+		block_init = block_end;
+	}
+	if(block_end != a->getAmountTransactions()) {
+		//cout << "thread: " << block_end << ", " << a->getAmountTransactions() << endl;
+		threads.insert(threads.end(), new thread(run_apriori_genThreaded, new_candidate/*_thread[threads.size()]*/, a, &last_large, block_end, a->getAmountTransactions()));
+	}
+	
+	for(auto & t : threads)
+		t->join();
+
+	for(auto & t : threads)
+		delete(t);
+		
+	/*for(auto & t : new_candidate_thread) {
+		new_candidate->getItemSets().insert(new_candidate->getItemSets().end(), t->getItemSets().begin(), t->getItemSets().end());
+		t->getItemSets().clear();
+		delete(t);
+	}*/
+		
+	return new_candidate;
+}
+
 LargeItemSet * CandidateItemSet::subset(LargeItemSet * a, vector <pair <string, string>> * normalized_transactions, unsigned int support) {
 	for(auto &i: a->getItemSets()) {
 		ItemSet * e = new ItemSet(i);
@@ -99,7 +213,7 @@ LargeItemSet * CandidateItemSet::subset(LargeItemSet * a, vector <pair <string, 
 	LargeItemSet * new_large = new LargeItemSet(a->getIteration());
 
 	unsigned int tests = 0;
-	cout << "normalized_transactions->size() = " << normalized_transactions->size() << endl;
+	//cout << "normalized_transactions->size() = " << normalized_transactions->size() << endl;
 
 	unsigned int j=0;
 	for(unsigned int i=0; i < normalized_transactions->size(); i=j) { //will scan transaction by transaction
@@ -119,25 +233,11 @@ LargeItemSet * CandidateItemSet::subset(LargeItemSet * a, vector <pair <string, 
 		root->transactionScan(&transaction);
 	}
 	
-	cout << "transactions tested = " << tests << endl;
+	//cout << "transactions tested = " << tests << endl;
 	
 	root->grabMinimumSupport(new_large, support);
 	
 	return new_large;
-}
-
-void run_threaded(Node * root, vector <pair <string, string>> * normalized_transactions, unsigned int rangeLow, unsigned int rangeHigh) {
-	unsigned int j=0;
-	for(unsigned int i=rangeLow; i < rangeHigh; i=j) { //will scan transaction by transaction
-		for(j=i; j < normalized_transactions->size() && (*normalized_transactions)[j].first == (*normalized_transactions)[i].first; ++j);
-		
-		vector<pair <string, string>>::const_iterator first = normalized_transactions->begin() + i; //get the first element in transaction
-		vector<pair <string, string>>::const_iterator last = normalized_transactions->begin() + j; //get the last element in transaction
-		
-		vector<pair <string, string>> transaction(first, last);
-		
-		root->transactionScan(&transaction);
-	}
 }
 
 vector<pair <unsigned int, unsigned int>> & startThreadSettings(vector <pair <string, string>> * normalized_transactions) {
@@ -147,7 +247,7 @@ vector<pair <unsigned int, unsigned int>> & startThreadSettings(vector <pair <st
 		return thread_blocks;
 		
 	unsigned concurentThreadsSupported = thread::hardware_concurrency();
-	cout << "--THREADS INFO--" << endl << concurentThreadsSupported << " concurrent threads will be used" << endl;
+	//cout << "--THREADS INFO--" << endl << concurentThreadsSupported << " concurrent threads will be used" << endl;
 	
 	unsigned int initial=0;
 	while(thread_blocks.size() < concurentThreadsSupported-1) {
@@ -163,11 +263,26 @@ vector<pair <unsigned int, unsigned int>> & startThreadSettings(vector <pair <st
 	thread_blocks.insert(thread_blocks.end(), pair<unsigned int, unsigned int>(initial, normalized_transactions->size()));
 	
 	for(auto & i: thread_blocks) {
-		cout << "thread has range: " << i.first << " to " << i.second << endl;
+		//cout << "subset thread has range: " << i.first << " to " << i.second << endl;
 	}
-	cout << "--THREADS INFO--" << endl;
+	//cout << "--THREADS INFO--" << endl;
 	
 	return thread_blocks;
+}
+
+void run_subsetThreaded(Node * root, vector <pair <string, string>> * normalized_transactions, unsigned int rangeLow, unsigned int rangeHigh) {
+	unsigned int j=0;
+	for(unsigned int i=rangeLow; i < rangeHigh; i=j) { //will scan transaction by transaction
+		for(j=i; j < normalized_transactions->size() && (*normalized_transactions)[j].first == (*normalized_transactions)[i].first; ++j);
+		
+		vector<pair <string, string>>::const_iterator first = normalized_transactions->begin() + i; //get the first element in transaction
+		vector<pair <string, string>>::const_iterator last = normalized_transactions->begin() + j; //get the last element in transaction
+		
+		vector<pair <string, string>> transaction(first, last);
+		
+		root->transactionScan(&transaction);
+	}
+	//cout << "thread exit" << endl;
 }
 
 LargeItemSet * CandidateItemSet::subsetThreaded(LargeItemSet * a, vector <pair <string, string>> * normalized_transactions, unsigned int support) {
@@ -182,7 +297,8 @@ LargeItemSet * CandidateItemSet::subsetThreaded(LargeItemSet * a, vector <pair <
 	vector<thread *> threads;
 	
 	for(unsigned int i=0; i<thread_blocks.size(); i++) {
-		threads.insert(threads.end(), new thread(run_threaded, root, normalized_transactions, thread_blocks[i].first, thread_blocks[i].second));
+		//cout << "thread : " << thread_blocks[i].first << " " << thread_blocks[i].second << endl;
+		threads.insert(threads.end(), new thread(run_subsetThreaded, root, normalized_transactions, thread_blocks[i].first, thread_blocks[i].second));
 	}
 	
 	for(auto & t: threads)
@@ -191,6 +307,7 @@ LargeItemSet * CandidateItemSet::subsetThreaded(LargeItemSet * a, vector <pair <
 	for(auto & t: threads)
 		delete(t);
 	
+	//cout << "scanning tree for support" << endl;
 	root->grabMinimumSupport(new_large, support);
 	
 	return new_large;
--- apriori/src/candidate/candidate.hpp
+++ apriori/src/candidate/candidate.hpp
@@ -28,9 +28,12 @@ class CandidateItemSet {
 		~CandidateItemSet();
 		
 		LargeItemSet * apriori_gen(LargeItemSet *);
+		LargeItemSet * apriori_genThreaded(LargeItemSet *);
 		LargeItemSet * subset(LargeItemSet * a, vector <pair <string, string>> * normalized_transactions, unsigned int);
 		LargeItemSet * subsetThreaded(LargeItemSet * a, vector <pair <string, string>> * normalized_transactions, unsigned int);
 };
 
-void run_threaded(Node *, vector <pair <string, string>> *, unsigned int, unsigned int);
+void run_apriori_genThreaded(LargeItemSet *, LargeItemSet *, vector<ItemSet *> *, unsigned int, unsigned int);
+
 vector<pair <unsigned int, unsigned int>> & startThreadSettings(vector <pair <string, string>> *);
+void run_subsetThreaded(Node *, vector <pair <string, string>> *, unsigned int, unsigned int);
